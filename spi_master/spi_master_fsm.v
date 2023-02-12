2ca6f7dce0588c1e617908bde5ed3bf37a985f09
Kayce Serafin <kcserafin@gmail.com>
2023-02-12 10:39:26 -0700

    add counter for instuction and data

--- spi_master/spi_master_fsm.v
+++ spi_master/spi_master_fsm.v
@@ -11,13 +11,22 @@ module spi_master_fsm(
     input get_rdid
     );
 
+    // Internal signals
+    reg[127:0]ascii_state, ascii_next_state; //for testbench annotation
     reg [2:0] state, next_state;
+    reg [3:0] count_inst;
+    reg [4:0] count_data;
+    reg send_inst_flag;
+    reg get_data_flag;
+    reg instruction_sent;
+    reg data_received;
+    reg cs;
 
     parameter idle = 3'b000;
     parameter assert_cs = 3'b001;
-    parameter send_inst = 3'b010;
+    parameter send_instruction = 3'b010;
     parameter get_data = 3'b011;
-    parameter deassert_cs = 3'b100;
+    parameter deAssert_cs = 3'b100;
 
     always @(posedge clk ) begin
         if (reset == 1'b1) state <= idle;
@@ -25,45 +34,74 @@ module spi_master_fsm(
     end
 
     always @(*) begin
-        // FIXME: initilize outputs here
+        send_inst_flag = 0;
+        get_data_flag = 0;
         case(state) 
-        idle:
+        idle: begin 
             if(get_rdid == 1) next_state = assert_cs;
-        assert_cs:
-            next_state = send_inst;
-        send_inst: 
-            // FIXME: this state should take 8 clks
-            next_state = get_data;
-        get_data:
-            // FIXME: this state should take 24 clks
-            next_state = deassert_cs;
-        deassert_cs:
+        end
+        assert_cs: begin 
+            cs = 1;
+            next_state = send_instruction;
+        end
+        send_instruction: begin  
+            send_inst_flag = 1;
+            count_inst = 4'b1000;
+            if (instruction_sent == 1) next_state = get_data;
+        end
+        get_data: begin 
+            get_data_flag = 1;
+            count_data = 5'b1_1000;
+            if (data_received == 1) next_state = deAssert_cs;
+        end
+        deAssert_cs: begin 
+            cs = 0;
             next_state = idle;
-        default:
+        end
+        default: begin 
             next_state = idle;
+        end
         endcase
     end
 
-    reg[119:0]ascii_state, ascii_next_state; //for annotation
+    // Send instruction
+    always @(posedge clk ) begin
+        if(send_inst_flag == 1) begin
+            // TODO: Send data here
+            count_inst <= count_inst - 1;
+            if(count_inst == 0) instruction_sent <= 1;
+        end
+    end
 
+    // Send data
+    always @(posedge clk ) begin
+        if(get_data_flag == 1) begin
+            // TODO: Send data here
+            count_data <= count_data - 1;
+            if(count_data == 0) data_received <= 1;
+        end
+    end
+
+    // For annotation on testbench
     always @(state) begin
         case(state) 
         idle: ascii_state = "idle";
         assert_cs: ascii_state = "assert_cs";
-        send_inst:  ascii_state = "send_inst";
+        send_instruction:  ascii_state = "send_instruction";
         get_data: ascii_state = "get_data";
-        deassert_cs: ascii_state = "deassert_cs";
+        deAssert_cs: ascii_state = "deAssert_cs";
         default: ascii_state = "default";
         endcase
     end
 
+    // For annotation on testbench
     always @(next_state) begin
         case(next_state) 
         idle: ascii_next_state = "idle";
         assert_cs: ascii_next_state = "assert_cs";
-        send_inst:  ascii_next_state = "send_inst";
+        send_instruction:  ascii_next_state = "send_instruction";
         get_data: ascii_next_state = "get_data";
-        deassert_cs: ascii_next_state = "deassert_cs";
+        deAssert_cs: ascii_next_state = "deAssert_cs";
         default: ascii_next_state = "default";
         endcase
     end
