2688323a2615f06ee467c937b468397e7d132ad0
Kayce Serafin <kcserafin@gmail.com>
2023-02-23 20:03:36 -0700

    add self checking test bench

--- rdid_instruction/one_shot_tb.v
+++ rdid_instruction/one_shot_tb.v
@@ -39,5 +39,39 @@ module one_shot_tb();
 
         #90 get_rdid_debounce = 0;
     end
+
+    // self checking testbench
+    integer testbench_error = 0;
+    reg test_signal = 0;
+
+    initial begin
+        #10 $display("*************** test begin *******************");
+        #102.5 test_signal = ~test_signal;
+            if(data_out !== 0) begin
+                $display("data out expected to be 0. Actual %d", data_out);
+                testbench_error = testbench_error + 1;
+            end
+        #1 test_signal = ~test_signal;
+            if(data_out !== 1) begin
+                $display("data out expected to be 1. Actual %d", data_out);
+                testbench_error = testbench_error + 1;
+            end
+        #16 test_signal = ~test_signal;
+            if(data_out !== 1) begin
+                $display("data out expected to be 1. Actual %d", data_out);
+                testbench_error = testbench_error + 1;
+            end
+        #1 test_signal = ~test_signal;
+            if(data_out !== 0) begin
+                $display("data out expected to be 0. Actual %d", data_out);
+                testbench_error = testbench_error + 1;
+            end
+    end
+
+    // end of simulation checks
+    initial begin
+        #300 if (testbench_error == 0) $display("Test passed!");
+        else $display("Test failed with %d errors", testbench_error);
+    end
    
 endmodule
