7ef44549a7c22e41486f6e57881fb3cc5879e96f
Kayce Serafin <kcserafin@gmail.com>
2023-03-05 14:04:27 -0700

    remove reset signal

--- rdid_instruction/debounce.v
+++ rdid_instruction/debounce.v
@@ -1,16 +1,15 @@
 module debounce ( 
 	input wire clk,
-    input wire reset, 
     input wire data_in, 
-    output reg data_debounced 
+    output reg data_debounced = 0
 	);
 
-	reg [15:0] change_reg;
+	reg [15:0] change_reg = 0;
 
-	always @(posedge clk or posedge reset) begin 
-		if (reset)
-			change_reg <= 16'b0;
-		else if (data_debounced && data_in)
+	always @(posedge clk) begin 
+		// if (reset)
+		// 	change_reg <= 16'b0;
+		if (data_debounced && data_in)
 			change_reg <= 16'b0;
 		else if (!data_debounced && !data_in)
 			change_reg <= 16'b0;
@@ -20,10 +19,8 @@ module debounce (
 			change_reg <= change_reg + 1'b1;
 	end
 
-	always @(posedge clk or posedge reset) begin
-		if (reset) 
-			data_debounced <= 1'b0;
-		else if (change_reg == 16'hFFFF)
+	always @(posedge clk) begin
+		if (change_reg == 16'hFFFF)
 			data_debounced <= data_in;
 	end 
 endmodule
--- rdid_instruction/debounce_tb.v
+++ rdid_instruction/debounce_tb.v
@@ -9,7 +9,6 @@
 module debounce_tb;
 
     // Inputs
-    reg reset;
     reg data_in;
     reg clk;
 
@@ -22,7 +21,6 @@ module debounce_tb;
     // Instantiate the Units Under Test (UUT)
     debounce debounce_UUT (
         .clk(clk),
-        .reset(reset),
         .data_in(data_in),
         .data_debounced(data_debounced)
     );
@@ -36,14 +34,11 @@ module debounce_tb;
     // Stimulus for UUT
 	initial begin
 		// Initialize Inputs
-		reset = 1;
         data_in = 0;
         button_push = 0;
-        
-		// Wait 100 ns for global reset to finish
-		#20;
-        reset = 0;
 
+        #20;
+        
         // simulate bouncing
         #0.5 data_in = 1;
         button_push = 1;
@@ -58,10 +53,8 @@ module debounce_tb;
         #0.5 data_in = 0;
         #0.5 data_in = 1;
         button_push = 0;
-        // end of simulated bounce
 
 	    #2000000 // 2ms
-        // simulate bouncing again
         button_push = 1;
         #0.5 data_in = 1;
         #0.5 data_in = 0;
@@ -79,14 +72,17 @@ module debounce_tb;
 
     // self checking testbench
 	integer testbench_error = 0;
+    reg test_signal = 0;
 
     initial begin
 		#10 $display("*************** test begin *******************");
-		#1310750 if(data_debounced !== 1) begin
+		#1310750  test_signal = ~test_signal; 
+            if(data_debounced !== 1) begin
             $display("Data_debounced expected to be 1. Actual %d", data_debounced);
             testbench_error = testbench_error + 1;
             end
-        #3310770 if(data_debounced !== 0) begin
+        #3310770 test_signal = ~test_signal; 
+            if(data_debounced !== 0) begin
             $display("Data_debounced expected to be 1. Actual %d", data_debounced);
             testbench_error = testbench_error + 1;
             end
@@ -94,7 +90,7 @@ module debounce_tb;
 
     // end of simulation checks
 	initial begin 
-		#4000 if (testbench_error == 0) $display("Test passed!");
+		#1600000 if (testbench_error == 0) $display("Test passed!");
 		else $display("Test failed with %d errors", testbench_error);
 	end
 endmodule
